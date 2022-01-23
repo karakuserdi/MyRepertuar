895c2c7b5587ad2ce4d1ce8618d14aaf4908686a
karakuserdi <karakuserdi@gmail.com>
2022-01-23 14:45:34 +0300

    v1.0.1

--- MyRepertuar/controllers/HomeViewController.swift
+++ MyRepertuar/controllers/HomeViewController.swift
@@ -13,17 +13,38 @@ class HomeViewController: UIViewController {
     var pageNumber:Int = 1
     var sanatciList = [SanatciList]()
     var artistNameAndId:(String,String)?
-
+    let letters = ["-","A","B","C","D","E","F","G","Ğ","H","İ","I","J","K","L","M","N","O","Ö","P","R","S","Ş","T","U","Ü","V","X","Y","Z"]
+    var selectedLetter = ""
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var letterCollectionView: UICollectionView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
+        
+        configureCollectionViewUI()
         fetchSanatciList()
 
+    }
+    
+    func configureCollectionViewUI(){
+        letterCollectionView.delegate = self
+        letterCollectionView.dataSource = self
+        letterCollectionView.showsHorizontalScrollIndicator = false
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumInteritemSpacing = 5
+        layout.scrollDirection = .horizontal
+        
+        let width = letterCollectionView.frame.size.height
+        let squareWidth = width - 25
+        layout.itemSize = CGSize(width: squareWidth, height: squareWidth)
+        letterCollectionView.collectionViewLayout = layout
     }
     
     func fetchSanatciList(){
@@ -32,16 +53,39 @@ class HomeViewController: UIViewController {
         }
         isLoad = true
         
-        RepertuarServices.shared.getSanatciList(page: pageNumber,search: "") { result in
+        if selectedLetter == "-"{
+            selectedLetter = ""
+        }
+
+        RepertuarServices.shared.getSanatciList(page: pageNumber,search: selectedLetter) { result in
             DispatchQueue.main.async {
                 for i in result{
                     self.sanatciList.append(i)
-                    
                 }
+                
                 self.tableView.reloadData()
                 self.isLoad = false
             }
         }
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return letters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "homeCollectionCell", for: indexPath) as! HomeCollectionCell
+        let letter = letters[indexPath.row]
+        cell.letterLabel.text = letter
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedLetter = letters[indexPath.row]
+        pageNumber = 1
+        sanatciList.removeAll()
+        fetchSanatciList()
     }
 }
 
@@ -53,9 +97,15 @@ extension HomeViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell", for: indexPath) as! HomeCell
         let sanatci = sanatciList[indexPath.row]
+        let firstChar = Array(sanatci.sanatciAdi)[0]
+        
+        cell.firstLetterOfArtistLabel.text = "\(firstChar)"
+        cell.totalNumberOfAlbumsLabel.text = "Toplam albüm sayısı: \(sanatci.albumCount)"
+        cell.totalNumberOfSongsLabel.text = "Toplam şarkı sayısı: \(sanatci.sarkiCount)"
+        cell.artistNameLabel.text = sanatci.sanatciAdi
         
         
-        cell.artistNames.text = sanatci.sanatciAdi
+        
         return cell
     }
     
@@ -65,6 +115,7 @@ extension HomeViewController:UITableViewDelegate,UITableViewDataSource{
             pageNumber += 1
             self.fetchSanatciList()
         }
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
--- MyRepertuar/controllers/view-cell/HomeCell.swift
+++ MyRepertuar/controllers/view-cell/HomeCell.swift
@@ -10,11 +10,21 @@ import UIKit
 class HomeCell: UITableViewCell {
     
     
-    @IBOutlet weak var artistNames: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artistNameView: UIView!
+    @IBOutlet weak var firstLetterOfArtistLabel: UILabel!
+    @IBOutlet weak var totalNumberOfSongsLabel: UILabel!
+    @IBOutlet weak var totalNumberOfAlbumsLabel: UILabel!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureUI()
         // Initialization code
+    }
+    
+    func configureUI(){
+        artistNameView.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
